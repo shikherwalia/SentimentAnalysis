e66f0de277b5c4f8f1edfad93c95d0640de01b4b
Shikher Walia <shikherwalia@gmail.com>
2017-12-20 16:44:50 +0530

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,23 +1,23 @@
-library(shiny) 
-
-
-shinyUI(fluidPage( 
-  titlePanel("Twitter Sentiment Analysis"), 
-  sidebarLayout( 
-    sidebarPanel( 
-      textInput("searchTerm", label = "Enter twitter search keywords below", value = "#srk"),
-      selectInput('plot_opt', 'Plot options', c('emotion', 'polarity'), selectize=TRUE), 
-      submitButton("View") 
-    ), 
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Sentiment analysis for the given word ",plotOutput("plot_emotion")),
-        tabPanel("Wordcloud", plotOutput("wordcloudform"))
-        
-      )
-      )
-      
-      ) 
-  ) 
-)
+library(shiny)
+library(twitteR)
+library(wordcloud)
+library(tm)
+shinyServer(function (input, output) {
+  rawData = reactive(function(){
+    tweets = searchTwitter(input$term, n=input$count, lang=input$lang)
+    tweets = twListToDF(tweets)
+  })
+  output$table = renderTable(function () {
+    head(rawData()[1],n=input$obs)
+  })
+  output$wordcl = renderPlot(function(){
+    tw.text = enc2native(rawData()$text)
+    tw.text = tolower(tw.text)
+    tw.text = removeWords(tw.text,c(stopwords(input$lang),"rt"))
+    tw.text = removePunctuation(tw.text,TRUE)
+    tw.text = unlist(strsplit(tw.text," "))
+    word = sort(table(tw.text),TRUE)
+    wordc = head(word,n=100)
+    wordcloud(names(wordc),wordc,random.color=TRUE,colors=rainbow(10),scale=c(15,2))
+  })
+})
